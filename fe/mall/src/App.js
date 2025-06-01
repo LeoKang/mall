e4f215e174fe082d01da57e37eae89676e800579
Seungiin Kang-Shingil <xeungjin@nate.com>
2025-06-01 17:14:14 +0900

    Main Page 추가

--- fe/mall/src/App.js
+++ fe/mall/src/App.js
@@ -1,7 +1,9 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import root from "./router/root";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
+  return <RouterProvider router={root} />;
 }
 
 export default App;

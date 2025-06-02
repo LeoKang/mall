fa65a2bbf177dd5321ceaa74802c38e8b712948c
Seungiin Kang-Shingil <xeungjin@nate.com>
2025-06-02 12:05:36 +0900

    2.8 중첩 라우팅의 분리와 리다이렉션(Redirection)

--- fe/mall/src/router/root.js
+++ fe/mall/src/router/root.js
@@ -1,4 +1,5 @@
 import { lazy, Suspense } from "react";
+import todoRouter from "./todoRouter";
 
 const { createBrowserRouter } = require("react-router-dom");
 
@@ -6,7 +7,6 @@ const Loading = <div>Loading...</div>;
 const Main = lazy(() => import("../pages/MainPage"));
 const About = lazy(() => import("../pages/AboutPage"));
 const TodoIndex = lazy(() => import("../pages/todo/IndexPage"));
-const TodoList = lazy(() => import("../pages/todo/ListPage"));
 
 const root = createBrowserRouter([
   {
@@ -32,16 +32,7 @@ const root = createBrowserRouter([
         <TodoIndex />
       </Suspense>
     ),
-    children: [
-      {
-        path: "list",
-        element: (
-          <Suspense fallback={Loading}>
-            <TodoList />
-          </Suspense>
-        ),
-      },
-    ],
+    children: todoRouter(),
   },
 ]);
 
